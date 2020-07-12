61d4ac907132a6eec30e87cabf400f0217c78726
Sky-Titan <er65119@naver.com>
2020-07-12 14:52:42 +0900

    .

---
+++ 02/js/change.js
@@ -0,0 +1,7 @@
+var heading = document.querySelector('#heading');
+		heading.onclick = function() {
+			if(heading.style.color == "red")
+			heading.style.color = "blue";
+			else
+			heading.style.color = "red";
+		}
